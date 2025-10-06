21103b623704ae4f22d85a9a9846911d69462927
Alejandro Canek <alejandro.canek.dev@gmail.com>
2025-10-06 08:30:35 -0600

    feat: add transitions

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,23 @@
 // @ts-check
 import { defineConfig } from "astro/config";
-
+import path from 'path';
+import { fileURLToPath } from 'url';
 import tailwindcss from "@tailwindcss/vite";
 
 import vercel from "@astrojs/vercel";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: tailwindcss(),
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, './src'),
+      }
+    }
   },
   adapter: vercel(),
   trailingSlash: "always",
